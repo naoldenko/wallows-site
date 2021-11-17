18bff253be67f86ac9241a9f2c723e90679f5b85
naoldenko <naoldenko@gmail.com>
2021-11-17 14:59:02 -0500

    connect overlay + home button

---
+++ js/connect.js
@@ -0,0 +1,7 @@
+function on() {
+    document.getElementById("connect_overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("connect_overlay").style.display = "none";
+  }
